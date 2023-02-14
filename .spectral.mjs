818721e4c4a5af9c88f7f4e1df4daeb27be52bbd
qa-jshhm-3-cvyc@example.com <qa-jshhm-3-cvyc@example.com>
2023-02-14 17:16:41 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://integration.stoplight-dev.com/api/v1/projects/cHJqOjEwNzY0NQ/spectral.js?branch_slug=main&token=0ed20fc6-27d3-4d72-a561-a29f98b79627";
+export default { extends: ruleset };
